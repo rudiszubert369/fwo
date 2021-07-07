cd4e27fe2874622d0b7bf9db98a71267f03e1e28
ernestodebesto <filip.sikor@gmail.comgitgit config --global user.email filip.sikor@gmail.com>
2021-07-07 15:53:16 +0200

    first commit

---
+++ gulpfile.js
@@ -0,0 +1,22 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var input='sass/**/*.scss';
+var output = 'css/';
+var sourcemaps = require('gulp-sourcemaps');
+var sassOptions = {
+  outputStyle: 'expanded'
+};
+
+gulp.task('sass', function(){
+  return gulp
+  .src(input)
+  .pipe(sourcemaps.init())
+  .pipe(sass(sassOptions).on('error', sass.logError))
+  .pipe(sourcemaps.write())
+  .pipe(gulp.dest(output))
+});
+
+gulp.task('watch', function() {
+  gulp.watch(input, ['sass', ])
+
+});
